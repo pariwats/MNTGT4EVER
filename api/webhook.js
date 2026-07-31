806ad09912782068e7a33553d5da165c58f99b9c
pariwats <dragonchaiwut@gmail.com>
2026-08-01 01:29:29 +0700

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,14 +1,19 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
 
-  if (req.method !== "POST") {
-    return res.status(200).send("Webhook OK");
+  if (req.method === "GET") {
+    return res.status(200).send("LINE Webhook Ready");
   }
 
-  const events = req.body.events;
+  if (req.method === "POST") {
 
-  console.log("LINE EVENT:", JSON.stringify(events));
+    console.log("LINE EVENT:");
+    console.log(JSON.stringify(req.body, null, 2));
 
-  return res.status(200).json({
-    message: "received"
-  });
+    return res.status(200).json({
+      status: "ok"
+    });
+
+  }
+
+  return res.status(405).send("Method Not Allowed");
 }
